0f5508d5f73a1b4f2e6eec9956ce40574bd950bc
RNXFreeze <m.raihannazhimoktana@gmail.com>
2025-05-18 23:08:47 +0700

    Stash : Pull

--- src/utils/DataStructure.java
+++ src/utils/DataStructure.java
@@ -7,10 +7,40 @@
 /* Tugas      : Tugas Kecil 3 - Strategi Algoritma (IF2211-24)                   */
 /* File Path  : Tucil3_13523021_13523095/src/utils/DataStructure.java            */
 /* Deskripsi  : F06 - Data Structure Utility                                     */
-/* PIC F06    : K02 - 13523095 - Rafif Farras                                    */
+/* PIC F06    : K01 - 13523021 - Muhammad Raihan Nazhim Oktana                   */
 
 package utils;
 
 public class DataStructure {
-    
+    private int[][] grid;
+    private int rows;
+    private int cols;
+
+    public DataStructure(int rows , int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new int[rows][cols];
+    }
+
+    public void setCell(int r, int c, int value) {
+        grid[r][c] = value;
+    }
+
+    public int getCell(int r, int c) {
+        return grid[r][c];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public enum Direction {UP , DOWN , LEFT , RIGHT}
+    public enum Orientation {HORIZONTAL , VERTICAL}
+    public record Pos(int r , int c) {
+
+    }
 }
