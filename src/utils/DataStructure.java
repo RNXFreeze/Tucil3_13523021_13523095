8412fd97dd68ddf5b51b7c528446d8ab6c3f4deb
RNXFreeze <m.raihannazhimoktana@gmail.com>
2025-05-18 22:38:02 +0700

    Init : All Files

--- src/utils/DataStructure.java
+++ src/utils/DataStructure.java
@@ -2,7 +2,7 @@
 /* Nama - 1   : Muhammad Raihan Nazhim Oktana                                    */
 /* NIM - 1    : K01 - 13523021 - Teknik Informatika (IF-Ganesha) ITB             */
 /* Nama - 2   : Rafif Farras                                                     */
-/* NIM - 2    : K01 - 13523095 - Teknik Informatika (IF-Ganesha) ITB             */
+/* NIM - 2    : K02 - 13523095 - Teknik Informatika (IF-Ganesha) ITB             */
 /* Tanggal    : Senin, 19 Mei 2025                                               */
 /* Tugas      : Tugas Kecil 3 - Strategi Algoritma (IF2211-24)                   */
 /* File Path  : Tucil3_13523021_13523095/src/utils/DataStructure.java            */
