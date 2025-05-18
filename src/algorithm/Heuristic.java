ae35e39aedfead8569c71389c473bf563b68c7a2
rafifrs <rafiffarras023@gmail.com>
2025-05-19 02:00:16 +0700

    fix: resolve conflict

---
+++ src/algorithm/Heuristic.java
@@ -0,0 +1,17 @@
+/* Kelompok   : Kelompok Tucil3_13523021_13523095                                */
+/* Nama - 1   : Muhammad Raihan Nazhim Oktana                                    */
+/* NIM - 1    : K01 - 13523021 - Teknik Informatika (IF-Ganesha) ITB             */
+/* Nama - 2   : Rafif Farras                                                     */
+/* NIM - 2    : K02 - 13523095 - Teknik Informatika (IF-Ganesha) ITB             */
+/* Tanggal    : Senin, 19 Mei 2025                                               */
+/* Tugas      : Tugas Kecil 3 - Strategi Algoritma (IF2211-24)                   */
+/* File Path  : Tucil3_13523021_13523095/src/algorithm/Heuristic.java            */
+/* Deskripsi  : F05 - Heuristic Algorithm                                        */
+/* PIC F05    : K01 - 13523021 - Muhammad Raihan Nazhim Oktana                   */
+
+package algorithm;
+import utils.*;
+
+public class Heuristic {
+    
+}
---
+++ src/game/GameLogic.java
@@ -0,0 +1,18 @@
+/* Kelompok   : Kelompok Tucil3_13523021_13523095                                */
+/* Nama - 1   : Muhammad Raihan Nazhim Oktana                                    */
+/* NIM - 1    : K01 - 13523021 - Teknik Informatika (IF-Ganesha) ITB             */
+/* Nama - 2   : Rafif Farras                                                     */
+/* NIM - 2    : K02 - 13523095 - Teknik Informatika (IF-Ganesha) ITB             */
+/* Tanggal    : Senin, 19 Mei 2025                                               */
+/* Tugas      : Tugas Kecil 3 - Strategi Algoritma (IF2211-24)                   */
+/* File Path  : Tucil3_13523021_13523095/src/game/GameLogic.java                 */
+/* Deskripsi  : F09 - Game Logic Program                                         */
+/* PIC F09    : K01 - 13523021 - Muhammad Raihan Nazhim Oktana                   */
+
+package game;
+import utils.*;
+import algorithm.*;
+
+public class GameLogic {
+    
+}
